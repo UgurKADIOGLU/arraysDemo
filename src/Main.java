1342012700c8bef5d1abdccddd603c1818649d5c
UgurKADIOGLU <ugrkadioglu@gmail.com>
2022-09-19 15:36:01 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String[] ogrenciler = new String[3];
+        ogrenciler[0] = "Uğur";
+        ogrenciler[1] = "Ahemet";
+        ogrenciler[2] = "Mehmet";
+
+        for (String ogrenci : ogrenciler
+        ) {
+            System.out.println(ogrenci);
+        }
+
+    }
+}
